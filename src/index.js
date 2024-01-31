c8612ba021a9916b0c35c27ab2ab361a81a18ed0
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2024-01-31 13:44:56 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import { ChatContextProvider } from './context/ChatContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -10,7 +11,9 @@ root.render(
   <AuthContextProvider>
     <ChatContextProvider>
       <React.StrictMode>
-        <App />
+        <BrowserRouter basename="/your-subdirectory">
+          <App />
+        </BrowserRouter>
       </React.StrictMode>
     </ChatContextProvider>
   </AuthContextProvider>
